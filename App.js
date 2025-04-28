77ae4b8d6ec02f16d02c42a3a9035db2da4ef1ab
Android Studio <devandroid@ort>
2025-04-28 09:59:28 -0300

    inicio

---
+++ App.js
@@ -0,0 +1,28 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import {styles} from './style.js';
+
+
+
+
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Prueba de creacion de este tp, Min, Selener y la changuita Kuro, Polshu crack</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text:{
+    color: "blue"
+  }
+});
---
+++ style.js
@@ -0,0 +1,18 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+
+
+
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#2616ae',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    text:{
+      color: "blue"
+    }
+  });
+  
